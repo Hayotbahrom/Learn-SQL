eeff2826f73bf3c5060bf61829ffaf2dce814c99
Xayotbaxrom.Nurdinov <hayotbahrom2002@gmail.com>
2026-01-22 15:43:21 +0500

    learned insert and On Conflict condition

--- PostgreSQL/DML_DDL_Commands.sql
+++ PostgreSQL/DML_DDL_Commands.sql
@@ -5,4 +5,11 @@ CREATE TABLE Passengers(
 	Age int check(age > 0)
 );
 
-SELECT * FROM Passengers;
+SELECT * FROM Passengers; 
+
+insert into Passengers (Id, firstname, lastname, age)
+values
+(1, 'Hayotbahrom', 'Nurdinov', 23),
+(2, 'Nuriddin', 'Mamadaliyev', 24)
+ON conflict (id) do update set firstname = excluded.firstname
+returning Id, firstname;

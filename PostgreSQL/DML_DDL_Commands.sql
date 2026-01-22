19fc65002fac6fadfee5b5404b93b9da70a20267
Xayotbaxrom.Nurdinov <hayotbahrom2002@gmail.com>
2026-01-22 17:27:51 +0500

    learn and practise DDL command, constraints and alter table commands

--- PostgreSQL/DML_DDL_Commands.sql
+++ PostgreSQL/DML_DDL_Commands.sql
@@ -13,3 +13,31 @@ values
 (2, 'Nuriddin', 'Mamadaliyev', 24)
 ON conflict (id) do update set firstname = excluded.firstname
 returning Id, firstname;
+
+create table Books(
+	id serial primary key,
+	title varchar
+);
+
+create table Author(
+	id serial primary key,
+	name varchar(20),
+	age int check(age > 18)
+);
+
+select * from books;
+
+select * from Authors;
+
+-- add new column to books table 
+alter table books add column author_id int;
+
+-- alter table name
+alter table Author RENAME TO Authors;
+
+-- add foreign key constraint to books table by author_id
+alter table books add constraint author_fkey foreign key(author_id)
+references Authors(id) on delete cascade;
+
+
+
